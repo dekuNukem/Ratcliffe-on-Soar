bc412e8dc5ae8d6720835be77cd7c29e378f82f0
dekuNukem <dekunukem@gmail.com>
2025-11-12 11:44:02 +0000

    updated push script

--- push.sh
+++ push.sh
@@ -1,14 +1,25 @@
-if [[ $# -eq 0 ]] ; then
-    echo 'error: please enter a commit comment'
-    exit 1
+#!/bin/bash
+
+# Check for commit message
+if [[ $# -eq 0 ]]; then
+    read -r -p "Enter a commit comment: " commit_message
+    if [[ -z "$commit_message" ]]; then
+        echo "error: commit comment cannot be empty"
+        exit 1
+    fi
+else
+    commit_message="$*"
 fi
 
+# Cleanup junk files
 find . -type f -name "*.DS_Store*" -exec rm -f {} \;
 find . -type f -name "*.eggs*" -exec rm -f {} \;
 find . -name "__pycache__" -exec rm -rf {} \;
 
+# Git workflow
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 
 git add --all
-git commit -m "$@"
+git commit -m "$commit_message"
 git push origin "$current_branch"
+
